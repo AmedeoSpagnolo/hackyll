17e5f6420f9648ba5b0110fdb2de47b5fce13eff
Amedeo SPAGNOLO <amedeo.spagnolo@gmail.com>
2018-08-28 00:50:03 +0800

    add page client chat

--- app/src/main/java/com/amedeospagnolo/hack/MainActivity.java
+++ app/src/main/java/com/amedeospagnolo/hack/MainActivity.java
@@ -112,6 +112,12 @@ public class MainActivity extends AppCompatActivity {
             context.startActivity(intent);
             overridePendingTransition(R.anim.enter_from_left, R.anim.exit_out_left);
             return true;
+        } else if (id == R.id.action_test_client){
+            Context context = MainActivity.this;
+            Intent intent = new Intent(MainActivity.this, ClientActivity.class);
+            context.startActivity(intent);
+            overridePendingTransition(R.anim.enter_from_left, R.anim.exit_out_left);
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
