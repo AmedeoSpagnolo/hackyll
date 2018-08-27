5ca065defd00625c2a3836d2f5dcc8dba789ca61
Amedeo SPAGNOLO <amedeo.spagnolo@gmail.com>
2018-08-28 01:15:25 +0800

    header page client

--- app/src/main/java/com/amedeospagnolo/hack/ClientActivity.java
+++ app/src/main/java/com/amedeospagnolo/hack/ClientActivity.java
@@ -1,9 +1,12 @@
 package com.amedeospagnolo.hack;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class ClientActivity extends AppCompatActivity {
@@ -11,7 +14,8 @@ public class ClientActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
+        setContentView(R.layout.activity_client);
+
 
         // ADD TOOLBAR
         Toolbar toolbar = findViewById(R.id.detail_toolbar);
@@ -25,6 +29,24 @@ public class ClientActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.enter_from_right, R.anim.exit_out_right);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_client, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_client_details) {
+//            do something special
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
