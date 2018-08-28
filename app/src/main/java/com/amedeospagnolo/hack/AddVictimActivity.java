b727fe8e709c66722b21fabbc1cd7533fdacdca1
Amedeo SPAGNOLO <amedeo.spagnolo@gmail.com>
2018-08-28 11:22:58 +0800

    clean and rename files

--- app/src/main/java/com/amedeospagnolo/hack/AddVictimActivity.java
+++ app/src/main/java/com/amedeospagnolo/hack/AddVictimActivity.java
@@ -11,7 +11,7 @@ public class AddVictimActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_victim);
+        setContentView(R.layout.activity_addvictim);
 
         // ADD TOOLBAR
         Toolbar toolbar = findViewById(R.id.detail_toolbar);
