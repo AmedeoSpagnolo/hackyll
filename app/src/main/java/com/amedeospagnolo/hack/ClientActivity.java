ff3f556ba80cb45f7edc63e0aa38e8277ff7536d
Amedeo SPAGNOLO <amedeo.spagnolo@gmail.com>
2018-08-30 11:59:05 +0800

    add fake data chat

--- app/src/main/java/com/amedeospagnolo/hack/ClientActivity.java
+++ app/src/main/java/com/amedeospagnolo/hack/ClientActivity.java
@@ -8,14 +8,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.security.AccessController.getContext;
 
 public class ClientActivity extends AppCompatActivity {
+    private EditText editText;
+    ArrayList<String> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
-
+        editText = findViewById(R.id.myNewMessageBox);
 
         // ADD TOOLBAR
         Toolbar toolbar = findViewById(R.id.detail_toolbar);
@@ -29,6 +41,34 @@ public class ClientActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.enter_from_right, R.anim.exit_out_right);
             }
         });
+
+        // fake data
+        ListView moviesList = findViewById(R.id.messages_view);
+
+        movies = new ArrayList<String>();
+        movies.add("X-Men");
+        movies.add("IRONMAN");
+        movies.add("SPIDY");
+        movies.add("NARNIA");
+        movies.add("LIONKING");
+        movies.add("AVENGERS");
+
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this, R.layout.chat_my_message, movies);
+        // Set The Adapter
+        moviesList.setAdapter(arrayAdapter);
+
+//        // register onClickListener to handle click events on each item
+//        moviesList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+//        {
+//            // argument position gives the index of item which is clicked
+//            public void onItemClick(AdapterView<?> arg0, View v,int position, long arg3)
+//            {
+//                String selectedmovie=movies.get(position);
+//                Toast.makeText(getApplicationContext(), "Movie Selected : "+selectedmovie,   Toast.LENGTH_LONG).show();
+//            }
+//        });
+
     }
 
     @Override
@@ -43,10 +83,17 @@ public class ClientActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_client_details) {
-//            do something special
+            // do something special
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        String message = editText.getText().toString();
+        if (message.length() > 0) {
+            editText.getText().clear();
+        }
     }
 
 }
