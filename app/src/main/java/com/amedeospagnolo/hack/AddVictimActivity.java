d1227db5cf064d5d67586dfb603bd591c13fd514
Amedeo SPAGNOLO <amedeo.spagnolo@gmail.com>
2018-09-03 19:30:12 +0800

    add spinner add client view

--- app/src/main/java/com/amedeospagnolo/hack/AddVictimActivity.java
+++ app/src/main/java/com/amedeospagnolo/hack/AddVictimActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddVictimActivity extends AppCompatActivity {
 
@@ -25,6 +28,22 @@ public class AddVictimActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.enter_from_right, R.anim.exit_out_right);
             }
         });
+
+        // spinner Server
+        Spinner spinnerServer = findViewById(R.id.add_client_spinner_server);
+        ArrayAdapter<CharSequence> adapterServer = ArrayAdapter.createFromResource(this,
+                R.array.server_array, android.R.layout.simple_spinner_item);
+        adapterServer.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerServer.setAdapter(adapterServer);
+
+        // Spinner Os
+        Spinner spinnerOs = findViewById(R.id.add_client_spinner_os);
+        ArrayAdapter<CharSequence> adapterOs = ArrayAdapter.createFromResource(this,
+                R.array.os_array, android.R.layout.simple_spinner_item);
+        adapterOs.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerOs.setAdapter(adapterOs);
+
     }
+
 
 }
