081016d887d44edede80ca133e8740a0214cc0b3
Tim W <timrlw@gmail.com>
2018-09-17 01:05:10 +0800

    add updating session list

--- app/src/main/java/com/msfrpc/Msf.java
+++ app/src/main/java/com/msfrpc/Msf.java
@@ -1,6 +1,14 @@
 package com.msfrpc;
 
 import com.amedeospagnolo.hack.MsfApplication;
+import com.msfrpc.model.RpcServer;
+import com.msfrpc.model.RpcSession;
+import com.msfrpc.model.Session;
+import com.msfrpc.rpc.RpcConnection;
+import com.msfrpc.rpc.RpcException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Msf {
 
@@ -8,6 +16,35 @@ public class Msf {
 
     public static Msf get() {
         return MsfApplication.Msf();
+    }
+
+    public void updateModels() {
+        for (RpcServer rpcServer : msfServerList.getServerList()) {
+            if (rpcServer.status != RpcServer.STATUS_CONNECTED) {
+                msfServerList.connectServer(rpcServer);
+            }
+            if (rpcServer.status == RpcServer.STATUS_CONNECTED) {
+                try {
+                    rpcServer.rpcConnection.updateModel();
+                } catch (RpcException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public List<RpcSession> getAllSessions() {
+        List<RpcSession> rpcSessionList = new ArrayList<>();
+        for (RpcServer rpcServer : msfServerList.getServerList()) {
+            RpcConnection rpcConnection = rpcServer.rpcConnection;
+            if (rpcConnection == null) {
+                continue;
+            }
+            for (Session session : rpcConnection.getModel().getSessions()) {
+                rpcSessionList.add(new RpcSession(rpcServer, session));
+            }
+        }
+        return rpcSessionList;
     }
 
 }
